c5e6468dca3981e8e881ba34a8e5136399ba3247
Just_A_Nobody5511 <giosaldua2@gmail.com>
2024-10-11 09:19:23 +0800

    Add files via upload

---
+++ Classroom_Attendance_System.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Classroom_Attendance_System {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int students;
+        int presentCount = 0;
+        int absentCount = 0;
+
+        //Total students
+        System.out.print("Enter the number of students in the class: ");
+        students = scnr.nextInt();
+
+        //Ask whether each student is present or absent
+        for (int i = 1; i <= students; i++) {
+            System.out.print("Is student " + i + " present? (Y/N): ");
+            String response = scnr.nextLine();
+            scnr.nextLine();
+
+            if (response.equalsIgnoreCase("Y")) {
+                presentCount++;
+            } else if (response.equalsIgnoreCase("N")) {
+                absentCount++;
+            } else {
+                System.out.println("Invalid input. Please enter 'Y' or 'N'.");
+                i--;
+            }
+        }
+
+        //Display
+        System.out.println("Total present: " + presentCount);
+        System.out.println("Total absent: " + absentCount);
+
+        scnr.close();
+    }
+}
